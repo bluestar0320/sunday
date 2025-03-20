0326ae6a7fc45f1a1cf1816845d613d65e8618be
bluestar0320 <qkdthdchkfdud@gmail.com>
2025-03-20 16:37:06 +0900

    test-03-20-목요일 첫번째

---
+++ 03numpy.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+print('1번째 zeros()실수형태 0. 숫자 발생')
+print(np.zeros(10))
+print()
+
+print(np.zeros((3,6)))
+print()
+
+
+
+
+
+print()
+print()
---
+++ 04zeroslike.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+print('1번째 zeros()실수형태 0. 숫자 발생')
+print(np.zeros(10))
+print()
+
+print(np.zeros((3,6)))
+print()
+
+data = [ 6, 0, 8, 9, 1.2 ]
+A = np.array(data)
+print(A)
+print(np.zeros_like(A))
+print()
+
+
+
+
+
+print()
+print()
